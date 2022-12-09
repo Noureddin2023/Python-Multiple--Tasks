0d7c8c38c92aa96c0107c376fb900fc9f06b2c7c
Noureddin2023 <nam2021sa@gmail.com>
2022-12-09 23:49:48 +0100

    add Task

--- Tasks 1.py
+++ Tasks 1.py
@@ -59,6 +59,8 @@ class Game:
         End = int(input('Enter End Number : '))
         for x in range(Start,End+1):
              for y in range(1,13):
+              print(f'{x}X{y}={x%y}')
+
 G1 = Game() 
               
   
